66a10ff59f6bacb75d28478226c08c1500a8793b
officialrajdeepsingh <radewala.2@gmail.com>
2023-01-10 22:48:35 +0530

    Create the post card for blog

---
+++ components/Post.tsx
@@ -0,0 +1,60 @@
+export default function Post(props:POSTProps) {
+    return (
+      <>
+        <div
+          className={`inline-flex my-24 flex-col sm:flex-col md:flex-col lg:flex-row xl:flex-row 2xl:flex-row justify-center  items-center mx-auto  text-[rgba(35,46,82,1)]  transition-all`}
+        >
+          
+          <div className="gap-3 px-0 sm:px-0 md:px-0 lg:px-3  xl:px-3  2xl:px-3  flex flex-col  items-start w-6/6 sm:w-6/6 md:w-[610px] lg:w-[610px] xl:w-[610px] 2xl:w-[610px]  mx-auto">
+            
+            <div className="gap-6 flex flex-col justify-center items-start">
+              
+              <div className="flex items-start text-center">
+                
+                
+                <div
+                  className={`rounded-l-md px-3 pt-2 pb-2.5 gap-2.5 flex justify-center items-center font-semibold bg-[rgba(235,242,254,1)] transition-all`}
+                >
+                  <p className="uppercase m-0 text-[13px] leading-[1.2]">
+                    {props.text}
+                  </p>
+
+                </div>
+
+                <div
+                  className="rounded-r-md px-3 pt-2 pb-2.5 bg-white gap-2.5 flex justify-center items-center font-medium w-[131px]"
+                >
+                  <time className="uppercase m-0 text-[13px] leading-[1.2]">
+                    {props.text1}
+                  </time>
+                </div>
+              </div>
+              <p
+                className="font-bold text-left m-0 text-[28px] leading-[1.3]"
+              >
+                {props.text2}
+              </p>
+            </div>
+            <p
+              className="text-lg font-normal leading-normal text-left m-0"
+            >
+              {props.text3}
+            </p>
+
+          </div>
+
+          <div style={{ "backgroundImage": `url(${props.image})` }} className={`rounded-lg bg-cover bg-no-repeat bg-center w-full sm:w-full md:w-full  lg:w-[300px] xl:w-[300px] 2xl:w-[300px] h-[210px] transition-all`} > </div>
+        </div>
+
+      </>
+    );
+  }
+  interface POSTProps {
+    text: string;
+    text1: string;
+    text2: string;
+    text3: string;
+    image:string;
+  }
+
+  // bg-[url('https://uortjlczjmucmpaqqhqm.supabase.co/storage/v1/object/public/firejet-converted-images/images/bd9f13ebb322ac0fa8c3d6174afbcd517a7da2c5.webp')]
