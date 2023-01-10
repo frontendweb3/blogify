01540ba473321ed80a99351bc56d831f34ea719a
officialrajdeepsingh <radewala.2@gmail.com>
2023-01-10 22:42:52 +0530

    Create the footer

---
+++ components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer(props) {
+    return (
+        <footer className="my-14 flex flex-col justify-center w-6/6 sm:w-[610px] md:w-[610px] lg:w-[610px] xl:w-[610px] 2xl:w-[610px] p-5 mx-auto  border-t-2 border-grey-100" >
+            <span className="text-center text-base"> CopyRight by <a href="https://github.com/frontendweb3"> Frontend web </a> </span>
+        </footer>
+    );
+}
+
+export default Footer;
