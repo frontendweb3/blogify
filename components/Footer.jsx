46d67081ccfb947d00e4e9dd177a05e17ba9cf78
officialrajdeepsingh <radewala.2@gmail.com>
2023-01-11 12:24:00 +0530

    Add the footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Footer(props) {
     return (
-        <footer className="my-14 flex flex-col justify-center w-6/6 sm:w-[610px] md:w-[610px] lg:w-[610px] xl:w-[610px] 2xl:w-[610px] p-5 mx-auto  border-t-2 border-grey-100" >
+        <footer className="my-16 flex flex-col justify-center w-6/6 sm:w-[610px] md:w-[610px] lg:w-[610px] xl:w-[610px] 2xl:w-[610px] p-6 mx-auto  border-t-2 border-grey-100" >
             <span className="text-center text-base"> CopyRight by <a href="https://github.com/frontendweb3"> Frontend web </a> </span>
         </footer>
     );
