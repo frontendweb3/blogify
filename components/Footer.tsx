602ceebb35b900a9e1dc202eebead4a66438e989
officialrajdeepsingh <radewala.2@gmail.com>
2023-01-11 23:14:31 +0530

    Update the footer

---
+++ components/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer() {
+    return (
+        <footer className="my-16 flex flex-col justify-center w-6/6 sm:w-[610px] md:w-[610px] lg:w-[610px] xl:w-[610px] 2xl:w-[610px] p-6 mx-auto  border-t-2 border-grey-100" >
+            <span className="text-center text-base"> Copyright by <a href="https://github.com/frontendweb3"> Frontend web </a> </span>
+        </footer>
+    );
+}
+
+export default Footer;
