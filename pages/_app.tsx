640f11f464b97a07504293bae7dfa7738f8098b0
officialrajdeepsingh <radewala.2@gmail.com>
2023-01-11 23:17:45 +0530

    update the layout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,11 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Footer from '../components/Footer'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+
+  return <> 
+            <Component {...pageProps} />
+            <Footer/>
+          </>
 }
