ac37493d4b3c09917a84eac9eb714b2eb9559a7f
officialrajdeepsingh <radewala.2@gmail.com>
2023-01-11 23:19:15 +0530

    Config the domain in next config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images:{
+    domains: ["uortjlczjmucmpaqqhqm.supabase.co","picsum.photos","random.imagecdn.app","source.unsplash.com"]
+  }
 }
 
 module.exports = nextConfig
