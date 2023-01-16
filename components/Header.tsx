d89321f3ebdb1979ef0966655edf07b221011160
officialrajdeepsingh <radewala.2@gmail.com>
2023-01-16 20:07:31 +0530

    fix the title and tag issue in tag page

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,6 @@
 import Nav from "./Nav";
 
-function Header(props:HeaderComponent) {
+function Header(props: HeaderComponent) {
 
   return (
     <header className="w-full flex flex-col py-5 bg-[rgba(35,46,82,1)]">
@@ -8,17 +8,22 @@ function Header(props:HeaderComponent) {
       <Nav />
 
       <div className="container flex flex-col mx-auto my-32 p-10">
-        
-        {
-          props.tag?  <p className="text-sm sm:text-sm md:text-base lg:text-base xl:text-base 2xl:text-base font-semibold text-white uppercase inline mt-8">
-          {props.tag?.replace("-"," ")}
-        </p> : " "
-        }
-        
 
-        <p className="font-bold text-left text-white inline mt-4 text-[32px] sm:text-[42px] md:text-[48px] lg:text-[52px] xl:text-[52px] 2xl:text-[52px]">
-          {props.title}
-        </p>
+
+        {
+          props.tag === "hidden" ? "" : <p className="text-sm sm:text-sm md:text-base lg:text-base xl:text-base 2xl:text-base font-semibold text-white uppercase inline mt-8">
+            {props.tag?.replace("-", " ")}
+          </p>
+        }
+
+
+        {
+          props.title === "hidden" ? " " : <p className="font-bold text-left text-white inline mt-4 text-[32px] sm:text-[42px] md:text-[48px] lg:text-[52px] xl:text-[52px] 2xl:text-[52px]">
+            {props.title}
+          </p>
+        }
+
+
 
       </div>
     </header>
@@ -26,8 +31,8 @@ function Header(props:HeaderComponent) {
 }
 
 Header.defaultProps = {
-  tag:"👋 Hello",
-  title: " Insights about my personal and work life, and the in-betweens"
+  tag: "👋 Rajdeep Singh",
+  title: "Start your front-end developer career journey with me."
 };
 
 interface HeaderComponent {
