765193d5c6052d358029655fc6686a591507b19e
officialrajdeepsingh <radewala.2@gmail.com>
2023-01-20 13:02:42 +0530

    Update the index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,38 +2,40 @@ import Header from "../components/Header"
 import Post from '../components/Post';
 import Newsletter from '../components/Newsletter';
 import ReadMore from '../components/ReadMore';
-import posts from '../data/post';
+import data from '../data/post';
 import dayjs from "dayjs";
 
-export default function Home() {
+export default function Home({ posts }: homePageProps) {
+
   return (
-      <>
+    <>
       <Header />
+
       <main className='container mx-auto flex flex-col p-3'>
 
         <p className="text-4xl inline font-bold text-left ml-0  sm:ml-0 md:ml-10 lg:ml-10 xl:ml-10 2xl:ml-24  my-16 leading-[normal] text-[rgba(35,46,82,1)]">
-            All posts
+          All posts
         </p>
 
         {
-          posts.map(
-            item =>  {
+          posts?.map(
+            (item: itemProps) => {
+              let GetDate = dayjs(item.date).format("DD-MMM , YYYY")
 
-            let GetDate=dayjs(item.date).format("DD-MMM , YYYY")         
-
-            return <Post key={item.id}
-            tag={item.tags[0]}
-            date={GetDate.toString()}
-            title={item.title}
-            description={item.description}
-            image={item.image}
-          />}
+              return <Post key={item.id}
+                tag={item.tags[0]}
+                date={GetDate.toString()}
+                title={item.title}
+                description={item.description}
+                image={item.image}
+              />
+            }
           )
         }
-        
-    
 
-      <ReadMore />
+
+
+        <ReadMore />
       </main>
 
 
@@ -42,4 +44,34 @@ export default function Home() {
   )
 }
 
-{/* https://cdn.pixabay.com/photo/2022/12/16/15/53/island-7660016_960_720.jpg */}
+export async function getStaticProps() {
+  return {
+    props: { posts: data },
+  }
+}
+
+interface homePageProps {
+  posts: {
+    map: any;
+    date: string;
+    title: string;
+    description: string;
+    image: string;
+    tags: string[];
+    author: string;
+    category: string[];
+    id: string;
+  }
+}
+
+interface itemProps {
+  date: string;
+  title: string;
+  description: string;
+  image: string;
+  tags: string[];
+  author: string;
+  category: string[];
+  id: string;
+
+}
