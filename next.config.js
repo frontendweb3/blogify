1aea548c30480f8256294f908be7dec78bce3bf1
officialrajdeepsingh <radewala.2@gmail.com>
2023-01-20 13:16:14 +0530

    Remove the domains in images config in  next.config.js

--- next.config.js
+++ next.config.js
@@ -2,7 +2,6 @@
 const nextConfig = {
   reactStrictMode: true,
   images:{
-    domains: ["uortjlczjmucmpaqqhqm.supabase.co","picsum.photos","random.imagecdn.app","source.unsplash.com"],
     unoptimized: true,
   },
   basePath: '/blogify',
