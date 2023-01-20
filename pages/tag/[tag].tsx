0ead15cd9cf82400cbc3bea154613acd7454307f
officialrajdeepsingh <radewala.2@gmail.com>
2023-01-20 13:03:09 +0530

    Update the tag page

--- pages/tag/[tag].tsx
+++ pages/tag/[tag].tsx
@@ -2,34 +2,22 @@ import React from 'react';
 import Header from '../../components/Header';
 import Post from '../../components/Post';
 import Newsletter from '../../components/Newsletter';
-import { useRouter } from 'next/router'
 import ReadMore from '../../components/ReadMore';
-import posts from '../../data/post';
+import data from '../../data/post';
 import dayjs from "dayjs";
 
-
-function Tag() {
-
-    const { query } = useRouter()
-
-    let item = query?.tag as string
-
-    let slug: string = item?.replaceAll("-", " ")
-
-    function filterPosts(params: { category: string[]; }) {
-        return params.category[0].toLowerCase() === slug
-    }
+function Tag({posts , tag }:tagPageProps ) {
 
     return (
 
         <>
-            <Header title='hidden' tag={`${slug}`} />
+            <Header title='hidden' tag={`${tag}`} />
 
             <main className='container mx-auto flex flex-col p-3'>
 
                 {
-                    posts.filter(filterPosts).map(
-                        item => {
+                    posts.map(
+                        (item:itemProps) => {
 
                             let GetDate = dayjs(item.date).format("DD-MMM , YYYY")
 
@@ -53,4 +41,55 @@ function Tag() {
     );
 }
 
+interface tagPageProps {
+  tag:string;
+  posts:{
+      map(arg0: (item: any) => JSX.Element): import("react").ReactNode;
+      filter(arg0: (_: any, i: any) => boolean): unknown;
+      date: string;
+      title: string;
+      description: string;
+      image:string;
+      tags:string[];
+      author:string;
+      category:string[];
+      id:string;
+  }
+}
+
+
+interface itemProps {
+  date: string;
+  title: string;
+  description: string;
+  image: string;
+  tags: string[];
+  author: string;
+  category: string[];
+  id: string;
+}
+
+export async function getStaticPaths() {
+    
+    const paths = data.map((item) => ({
+        params: { tag: item.category[0].toLowerCase().replaceAll(" ", "-") },
+      }))
+    
+      
+    return {
+      paths: paths,
+      fallback: false,
+    }
+}
+  
+export async function getStaticProps(context: { params: { tag: string; }; }) {
+  
+    const { params: { tag } } = context
+  
+    const posts = data.filter((item) => item.category[0].toLowerCase().replaceAll(" ", "-") === tag)
+  
+    return {
+      props: { posts ,tag},
+    }
+}
 export default Tag;
