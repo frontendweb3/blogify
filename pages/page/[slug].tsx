86c5ed0edad74ae99318aeee77a62997f9dea220
officialrajdeepsingh <radewala.2@gmail.com>
2023-01-22 21:06:49 +0530

    Update the about

---
+++ pages/page/[slug].tsx
@@ -0,0 +1,59 @@
+import React from 'react';
+
+import Header from '../../components/Header';
+
+
+function Pages() {
+
+    return (
+        <>
+            <Header tag='hidden' title='About us' />
+            <div className="my-12 prose prose-stone lg:prose-lg mx-auto">
+
+                <p className="mb-3 font-light text-gray-500 dark:text-gray-400 first-line:uppercase first-line:tracking-widest first-letter:text-7xl first-letter:font-bold first-letter:text-gray-900 dark:first-letter:text-gray-100 first-letter:mr-3 first-letter:float-left">
+                    Track work across the enterprise through an open, collaborative platform. Link issues across Jira and ingest data from other software development tools, so your IT support and operations teams have richer contextual information to rapidly respond to requests, incidents, and changes.
+                </p>
+                
+                <p className="font-light text-gray-500 dark:text-gray-400">
+                    Deliver great service experiences fast - without the complexity of traditional ITSM solutions.Accelerate critical development work, eliminate toil, and deploy changes with ease, with a complete audit trail for every change.
+                </p>
+
+
+                <p className="mb-3 font-light text-gray-500 dark:text-gray-400 ">
+                    Design comps, layouts, wireframes—will your clients accept that you go about things the facile way? Authorities in our business will tell in no uncertain terms that Lorem Ipsum is that huge, huge no no to forswear forever.
+                </p>
+
+                <p className="mb-3 font-light text-gray-500 dark:text-gray-400 ">
+                    Not so fast, I'd say, there are some redeeming factors in favor of greeking text, as its use is merely the symptom of a worse problem to take into consideration.
+                </p>
+
+                <p className="mb-3 font-light text-gray-500 dark:text-gray-400 ">
+                    The toppings you may chose for that TV dinner pizza slice when you forgot to shop for foods, the paint you may slap on your face to impress the new boss is your business. But what about your daily bread?
+                </p>
+
+
+                <p className="font-light text-gray-500 dark:text-gray-400" >
+                    The toppings you may chose for that TV dinner pizza slice when you forgot to shop for foods, the paint you may slap on your face to impress the new boss is your business. But what about your daily bread? Not so fast, I'd say, there are some redeeming factors in favor of greeking text, as its use is merely the symptom of a worse problem to take into consideration.
+                </p>
+
+                <p className="font-light text-gray-500 dark:text-gray-400" >
+                    Design comps, layouts, wireframes—will your clients accept that you go about things the facile way? Authorities in our business will tell in no uncertain terms that Lorem Ipsum is that huge, huge no no to forswear forever.
+                </p>
+
+
+                <p className="font-light text-gray-500 dark:text-gray-400">
+                    Design comps, layouts, wireframes—will your clients accept that you go about things the facile way? Authorities in our business will tell in no uncertain terms that Lorem Ipsum is that huge, huge no no to forswear forever.
+                </p>
+
+                <p className="font-light text-gray-500 dark:text-gray-400">                    
+                    The toppings you may chose for that TV dinner pizza slice when you forgot to shop for foods, the paint you may slap on your face to impress the new boss is your business. But what about your daily bread? Not so fast, I'd say, there are some redeeming factors in favor of greeking text, as its use is merely the symptom of a worse problem to take into consideration. 
+                </p>
+
+                <p className="font-light text-gray-500 dark:text-gray-400">Not so fast, I'd say, there are some redeeming factors in favor of greeking text, as its use is merely the symptom of a worse problem to take into consideration.esign comps, layouts, wireframes—will your clients accept that you go about things the facile way? Authorities in our business will tell in no uncertain terms that Lorem Ipsum is that huge, huge no no to forswear forever.
+                </p>
+            </div>
+        </>
+    );
+}
+
+export default Pages;
