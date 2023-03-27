9133c0eeb6dfed45a87d88d0f00aabea84573b25
officialrajdeepsingh <radewala.2@gmail.com>
2023-03-27 15:17:38 +0530

    remove the base path

--- next.config.js
+++ next.config.js
@@ -1,10 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images:{
+  images: {
     unoptimized: true,
-  },
-  //  basePath: '/blogify',
+  }
 }
 
 module.exports = nextConfig
