0c2275fb56e9275c7b063336d2a9318fe208bf4b
Takashi Ando <dodo5522@gmail.com>
2024-06-09 18:51:59 +0900

    [env] Manage author with Tina CMS

--- tina/config.ts
+++ tina/config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "tinacms";
 import Post from './collections/post';
+import Author from './collections/author';
 
 // Your hosting provider likely exposes this as an environment variable
 const branch =
@@ -29,6 +30,7 @@ export default defineConfig({
   // See docs on content modeling for more info on how to setup new content models: https://tina.io/docs/schema/
   schema: {
     collections: [
+      Author,
       Post,
     ],
   },
