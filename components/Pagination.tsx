0f5b0b873f6013b3a1ad4089d98eb22cedfa224c
Rajdeep Singh <radewala.2@gmail.com>
2024-11-30 18:23:50 +0530

    Update the Pagination UI

--- components/Pagination.tsx
+++ components/Pagination.tsx
@@ -1,29 +1,34 @@
 import React from 'react';
-import Link from 'next/link';
+import {
+  Pagination as PaginationBox,
+  PaginationContent,
+  PaginationItem,
+  PaginationNext,
+  PaginationPrevious,
+} from "@/components/ui/pagination";
+import { Label } from "@/components/ui/label"
 
-function Pagination() {
-
+export function Pagination() {
 
   return (
-    <div className="flex flex-col sm:flex-col md:flex-col lg:flex-row xl:flex-row 2xl:flex-row items-center w-5/6 sm:w-5/6 md:w-4/6 lg:w-4/6 xl:w-4/6 2xl:w-3/6 justify-evenly mx-auto">
+    <div className="my-8 grid gap-5 grid-cols-1 sm:grid-cols-2 items-center mx-auto">
 
-      <span className="text-sm text-gray-700 dark:text-gray-400">
-        Showing <span className="font-semibold text-gray-900 dark:text-white">1</span> to <span className="font-semibold text-gray-900 dark:text-white">10</span> of <span className="font-semibold text-gray-900 dark:text-white">100</span> Entries
-      </span>
+      <Label className="text-sm text-center"> Showing 1 to 10 of 100 Entries </Label>
 
-      <div className="inline-flex justify-evenly w-5/6 sm:w-5/6 md:w-3/6 lg:w-3/6 xl:w-4/6 2xl:w-2/6">
+      <PaginationBox>
+        <PaginationContent>
 
-        <Link href={"/"} className="h-10 mx-auto  w-[100px] my-14 gap-2.5 inline-flex flex-col justify-center items-center rounded-lg text-center font-medium bg-[rgba(235,242,254,1)] text-[rgba(35,46,82,1)]">
-          Prev
-        </Link>
+          <PaginationItem>
+            <PaginationPrevious href="#" />
+          </PaginationItem>
 
-        <Link href={"/"} className="h-10 mx-auto  w-[100px] my-14 gap-2.5 inline-flex flex-col justify-center items-center rounded-lg text-center font-medium bg-[rgba(235,242,254,1)] text-[rgba(35,46,82,1)]">
-          Next
-        </Link>
-      </div>
+          <PaginationItem>
+            <PaginationNext href="#" />
+          </PaginationItem>
+
+        </PaginationContent>
+      </PaginationBox>
+
     </div>
-
   );
 }
-
-export default Pagination;
