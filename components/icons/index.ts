47e2e6f3ed4a4d5a8995a24ca081b51a4d05cb86
Rajdeep Singh <radewala.2@gmail.com>
2024-11-30 18:22:10 +0530

    icons of blogify

---
+++ components/icons/index.ts
@@ -0,0 +1,6 @@
+export { Github } from "@/components/icons/github"
+export { Facebook } from "@/components/icons/facebook"
+export { Instagram } from "@/components/icons/instagram"
+export { Twitter } from "@/components/icons/twitter"
+export { Linkedin } from "@/components/icons/linkedin"
+export { Logo } from "@/components/icons/logo"
