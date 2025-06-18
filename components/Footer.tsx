d6fe19918f0f96a1ec172c35c90f935e765f06e7
Rajdeep Singh <radewala.2@gmail.com>
2025-06-18 20:22:37 +0530

    rewrite the ui

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,17 +1,32 @@
-import Link from '@/node_modules/next/link';
-import React from 'react';
-import { Button } from '@/components/ui/button';
+import Link from "next/link";
 
-function Footer() {
+interface Footer2Props {
+  copyright?: string;
+  bottomLinks?: {
+    text: string;
+    url: string;
+  }[];
+}
+
+export function Footer({
+  copyright = "© 2025 Blogify. All rights reserved.",
+  bottomLinks = [
+    { text: "Terms and Conditions", url: "/pages/term-and-conditions" },
+    { text: "Privacy Policy", url: "/pages/privacy-policy" },
+  ],
+}: Footer2Props) {
   return (
-    <footer className="container my-16 flex flex-col justify-center p-6 mx-auto border-t-2 border-border" >
-      <Button variant={"link"} className="text-md !mx-0 !px-0 mt-3" asChild>
-        <Link className='text-center' target={"_blank"} href={"https://github.com/frontendweb3"}>
-          CopyRight By @Frontend web
-        </Link>
-      </Button>
+    <footer className="container">
+      <div className="my-14 flex flex-col items-center justify-between gap-4 border-t pt-8 text-sm font-medium sm:flex-row">
+        <p>{copyright}</p>
+        <ul className="flex gap-4">
+          {bottomLinks.map((link, linkIdx) => (
+            <li key={linkIdx} className="underline hover:text-primary">
+              <Link href={link.url}>{link.text}</Link>
+            </li>
+          ))}
+        </ul>
+      </div>
     </footer>
   );
 }
-
-export default Footer;
