8ad29d7550470be6d96cd839dc29993ec67a45c6
Rajdeep Singh <radewala.2@gmail.com>
2025-06-19 17:28:27 +0530

    redesign the 404 ui

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,19 +1,32 @@
-import { Button } from '@/components/ui/button'
-import { Card, CardContent, CardDescription, CardTitle } from '@/components/ui/card'
-import Link from '@/node_modules/next/link'
-import React from 'react'
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardTitle,
+} from "@/components/ui/card";
+import Link from "@/node_modules/next/link";
+import { Home } from "lucide-react";
+import React from "react";
 
 export default function Notfound() {
   return (
-    <Card className="border-none shadow-none mt-16">
-      <CardContent className="mx-auto flex w-full flex-col items-center text-center">
-        <CardTitle className="my-4 text-6xl font-bold md:text-7xl">404 Error</CardTitle>
-        <CardDescription className="mx-auto mb-5 text-lg md:mb-6 lg:mb-8"> Commodo, consequat turpis placerat ultrices sapien, tortor tincidunt. Sit quisque est metus auctor sed turpis lectus quis. </CardDescription>
-        <Button className="mt-4 rounded-2xl" asChild>
-          <Link href="/"> Back Home </Link>
+    <Card className="mx-auto mt-16 max-w-7xl border-none shadow-none">
+      <CardContent className="mx-auto flex w-full flex-col items-center py-12 text-center">
+        <CardTitle className="my-4 text-6xl font-bold md:text-[140px]">
+          404
+        </CardTitle>
+        <CardDescription className="mx-auto mb-5 text-xl md:mb-6 lg:mb-8">
+          {" "}
+          Something has gone wrong; please return to the Home page.
+        </CardDescription>
+        <Button className="mt-4 rounded-none" size={"lg"} asChild>
+          <Link href="/">
+            {" "}
+            <Home /> Back to Home{" "}
+          </Link>
         </Button>
       </CardContent>
     </Card>
-  )
+  );
 }
-
