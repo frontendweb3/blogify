5655d81196d08cc568238f332a5c7d9e04fcc41f
Rajdeep Singh <radewala.2@gmail.com>
2025-06-19 17:44:16 +0530

    redesign the ui

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,20 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
-import { Header } from "@/components/Header";
-import Footer from '@/components/Footer';
+import { Footer } from "@/components/Footer";
 import { ThemeProvider } from "@/components/theme/theme-provider";
+import {
+  SidebarInset,
+  SidebarProvider,
+  SidebarRail,
+  SidebarTrigger,
+} from "@/components/ui/sidebar";
+import { SidebarApp } from "@/components/Sidebar";
+import { cookies } from "next/headers";
 import "./globals.css";
+import { SidebarButton } from "@/components/sidebar_button";
+import { ChevronRightIcon } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import { SidebarClose } from "@/components/sidebar_close";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -16,28 +27,37 @@ const geistMono = localFont({
 
 export const metadata: Metadata = {
   title: "Home | Blogify",
-  description: "Blogify is an open-source nextjs blog template design with tailwind CSS.",
+  description:
+    "Blogify is an open-source nextjs blog template design with tailwind CSS.",
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const cookieStore = await cookies();
+  const defaultOpen = cookieStore.get("sidebar_state")?.value === "false";
+
   return (
-    <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}  dark:bg-grid-small-white/5 bg-grid-small-black/5`}>
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
-        >
-          <Header />
-          {children}
+    <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+      <html lang="en">
+        <body className={`${geistSans.variable} ${geistMono.variable}`}>
+          <SidebarProvider
+            className="flex flex-col sm:flex-row"
+            defaultOpen={defaultOpen}
+          >
+            <SidebarApp />
+            <SidebarInset>
+              <SidebarButton />
+              <main className="container mx-auto max-w-6xl p-4">
+                {children}
+              </main>
+            </SidebarInset>
+          </SidebarProvider>
           <Footer />
-        </ThemeProvider>
-      </body>
-    </html>
+        </body>
+      </html>
+    </ThemeProvider>
   );
 }
